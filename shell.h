a64578a23634042c78462c91befaf4cfdaf5beab
moluno_xiii <molunoprogress@gmail.com>
2023-11-04 03:48:10 -0700

    Added shell.h file

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys.stat.h>
+#include <string.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <signal.h>
+
+extern char **environ;
