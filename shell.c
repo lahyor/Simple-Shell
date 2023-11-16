9e24a7dde6bc39cb0e832f50db7cad0fa2d77fab
moluno_xiii <molunoprogress@gmail.com>
2023-11-15 16:41:17 -0800

    shell clone

--- shell.c
+++ shell.c
@@ -1,15 +1,15 @@
 #include "shell.h"
 
-#define PROMPT "simple_shell$ "
+#define PROMPT "simple_shell$"
 
 /**
  * interp - Entry point
- * @argc: Number of argumets
- * @argv: Array of argumets
+ * @argc: Number of artguments
+ * @argv: Array of arguments
  * Return: Always 0 (success)
  */
 
-int interp(int argc, char *argv[])
+int interp(int args, char *argv[])
 {
 	char *line = NULL;
 	size_t len = 0;
@@ -18,7 +18,7 @@ int interp(int argc, char *argv[])
 	while (1)
 	{
 		printf("%s", PROMPT);
-		ssize_t bytes_read = getline(&line, *len, stdin);
+		ssize_t bytes_read = getline(&line, &len, stdin);
 
 		if (bytes_read == -1)
 		{
@@ -27,30 +27,14 @@ int interp(int argc, char *argv[])
 		}
 
 		line[strlen(line) - 1] = '\0';
-		args = malloc(sizeof(char *) * MAX_ARGS);
-
-		if (args == NULL)
-		{
-			perror("malloc");
-			exit(EXIT_FAILURE);
-		}
-
-		char *token = strtok(line, " ");
-		int i = 0;
-
-		while (token != NULL)
-		{
-			args[i++] = token;
-			token = strtok(NULL, " ");
-		}
-		args[i] = NULL;
+		args = tokenize(line);
 
 		if (args[0] != NULL)
 		{
-			if (execve(args[0], args, NULL) == -1)
+			if (exec_command(args) == -1)
 			{
 				perror("simple_shell");
-				printg("%s", PROMPT);
+				printf("%s", PROMPT);
 				continue;
 			}
 		}
@@ -59,6 +43,63 @@ int interp(int argc, char *argv[])
 		free(line);
 		line = NULL;
 	}
-	return (0)
 
+	return (0);
+}
+
+/**
+ * tokenize - Tokenize input line
+ * @line: Input line
+ *
+ * Return: Array of tokens
+ */
+
+char **tokenize(char *line)
+{
+	if (args == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	char *token = strtok(line, " ");
+	int i = 0;
+
+	while (token != NULL)
+	{
+		args[i++] = token;
+		token = strtok(NULL, " ");
+	}
+	args[i] = NULL;
+
+	return(args);
+}
+
+/**
+ * exec_command - Execute the command
+ * @args: Array of command
+ *
+ * Return: Always 0 (success)
+ */
+
+int exec_command(char **args)
+{
+	pid_t pid = fork();
+
+	if (pid == 0)
+	{
+		if (execve(args[0], args, NULL) == -1)
+		{
+			exit(EXIT_FAILURE);
+		}
+	} else if (pid < 0)
+	{
+		perror("fork");
+		return (-1);
+	} else
+	{
+		wait(NULL);
+	}
+
+	return (0);
 }
