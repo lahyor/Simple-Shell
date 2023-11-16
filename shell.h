310f259de3f730e3cf17d4f7ceb3465692bbcce4
moluno_xiii <molunoprogress@gmail.com>
2023-11-15 17:02:14 -0800

    edit shell.h

--- shell.h
+++ shell.h
@@ -10,7 +10,14 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <signal.h>
-
-extern char **environ;
+#define BUFFER_SIZE 1024
 
 int hello_betty(void);
+char *_getinput();
+int _strcmp(const char *str1, const char *str2);
+int _strlen(char *str);
+int _strncpy(char *dest, char *src, int size);
+void *_memcpy(void *dest, void *src, size_t n);
+void *_realloc(void *ptr, size_t size);
+
+#endif
